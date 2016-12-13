633b33852cd23282af01ebff031a316c45b6876d
Margaret Thomas <magz.thomas@gmail.com>
2016-12-13 16:43:58 +0200

    Create model for the Values Controller

---
+++ DemoWebApp/DemoWebApp.Data/Models/ValuesModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DemoWebApp.Data.Models
+{
+    public class ValuesModel
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+    }
+}
