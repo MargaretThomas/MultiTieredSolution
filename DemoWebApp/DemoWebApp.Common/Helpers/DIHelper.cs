4702ae8752f0cd0b2e7c929b7c84d12f1e612ed9
Margaret Thomas <magz.thomas@gmail.com>
2016-12-13 16:46:01 +0200

    Created AppElasticClient and registered it in the DIHelper

--- DemoWebApp/DemoWebApp.Common/Helpers/DIHelper.cs
+++ DemoWebApp/DemoWebApp.Common/Helpers/DIHelper.cs
@@ -1,6 +1,8 @@
 ﻿using DemoWebApp.ApplicationLogic.Interfaces;
 using DemoWebApp.ApplicationLogic.Services;
+using DemoWebApp.Data;
 using Microsoft.Practices.Unity;
+using Nest;
 
 namespace DemoWebApp.Common.Helpers
 {
@@ -9,6 +11,7 @@ namespace DemoWebApp.Common.Helpers
         public static void RegisterComponents<T>(UnityContainer container) where T : LifetimeManager, new()
         {
             container.RegisterType<IValues, ValuesService>(new T());
+            container.RegisterType<IElasticClient, AppElasticClient>(new T());
         }
     }
 }
---
+++ DemoWebApp/DemoWebApp.Data/AppElasticClient.cs
@@ -0,0 +1,14 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Data
+{
+    public class AppElasticClient : ElasticClient
+    {
+        AppElasticClient() : base (new Uri("htpp://localhost:9200")) { }
+    }
+}
